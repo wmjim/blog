5bad102e8e48beba13d7e2568dd229a25a871132
meng.wang <meng.w1016@outlook.com>
2026-06-19 13:50:43 +0800

    功能：移除过时的说说数据，保留最新记录

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -4,24 +4,24 @@ export default {
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
+    // {
+    //   "date": "2025-02-12 19:36:16",
+    //   "tags": [
+    //     "树",
+    //     "夕阳"
+    //   ],
+    //   "content": "好美🌲<p class=\"vh-img-flex\"><img src=\"https://i0.wp.com/shp.qpic.cn/collector/1655466387/937ec070-8448-4c7b-9c8b-abd41ce892cb/0\"></p>"
+    // },
+    // {
+    //   "date": "2024-10-08 18:18:18",
+    //   "tags": [
+    //     "日常",
+    //     "工作"
+    //   ],
+    //   "content": "下班！"
+    // },
     {
-      "date": "2025-02-12 19:36:16",
-      "tags": [
-        "树",
-        "夕阳"
-      ],
-      "content": "好美🌲<p class=\"vh-img-flex\"><img src=\"https://i0.wp.com/shp.qpic.cn/collector/1655466387/937ec070-8448-4c7b-9c8b-abd41ce892cb/0\"></p>"
-    },
-    {
-      "date": "2024-10-08 18:18:18",
-      "tags": [
-        "日常",
-        "工作"
-      ],
-      "content": "下班！"
-    },
-    {
-      "date": "2024-10-05 16:16:06",
+      "date": "2026-06-19 08:48:06",
       "tags": [
         "日常"
       ],
