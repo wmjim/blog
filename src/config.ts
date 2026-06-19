65f79da92060aef0cc563b4390c09d7d0bce4fa0
meng.wang <meng.w1016@outlook.com>
2026-06-19 08:43:29 +0800

    功能：新增 Email 图标并更新链接至联系邮箱

--- src/config.ts
+++ src/config.ts
@@ -63,7 +63,7 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: 'Github', link: 'https://github.com/wmjim', icon: 'WebSite_github' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
+    { text: 'Email', link: 'mailto:meng.w1016@outlook.com', icon: 'WebSite_email' },
     { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
     { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
     { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
