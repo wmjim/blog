19727aacbd6de54faa9e714ecb0cb62720a89424
meng.wang <meng.w1016@outlook.com>
2026-09-01 18:49:04 +0800

    博客：更新网站标题、作者信息和座右铭，新增社交链接图标

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   // 网站标题
-  Title: "海上一孤舟",
+  Title: "Viod",
   // 网站地址
   Site: 'https://wmjim.github.io/blog',
   // 网站副标题
@@ -8,11 +8,11 @@ export default {
   // 网站描述
   Description: '专注于分享和总结，记录我的学习、感想和生活点滴.',
   // 网站作者
-  Author: '海上一孤舟',
+  Author: 'Viod',
   // 作者头像
   Avatar: import.meta.env.BASE_URL + 'assets/images/avator.jpg',
   // 网站座右铭
-  Motto: '有利的情况和主动的恢复，产生于“再坚持一下”的努力之中。',
+  Motto: 'Be the change you wish to see in the world.',
   // 网站侧边栏公告 (不填写即不开启)
   Tips: '<p>欢迎光临我的博客 🎉</p><p>这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖</p>',
   // 首页打字机文案列表
@@ -63,9 +63,8 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: 'Github', link: 'https://github.com/wmjim', icon: 'WebSite_github' },
+    { text: 'X', link: 'https://x.com/Voidy7rp', icon: 'WebSite_x' },
     { text: 'Email', link: 'mailto:meng.w1016@outlook.com', icon: 'WebSite_email' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
     { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
   ],
   // 侧边栏展示
