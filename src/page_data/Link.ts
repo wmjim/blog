0e573f1438b4100ba1858f675f20d99216d775c8
meng.wang <meng.w1016@outlook.com>
2026-09-03 15:43:56 +0800

    功能：更新 Link 组件中的描述信息，提供更详细的个人介绍

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -7,13 +7,37 @@ export default {
       "name": "史帙",
       "link": "https://daybook.page/",
       "avatar": "https://daybook.page/images/avatar/shelby.jpg",
-      "descr": "笑一个吧，功成名就不是目的."
+      "descr": "这是一个由 Go 构建的静态博客，也是我用来托管 Obsidian 笔记的个人知识库。"
     },
     {
       "name": "韩小韩博客",
       "link": "https://www.vvhan.com",
       "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640",
-      "descr": "运气是计划之外的东西."
-    }
+      "descr": "我是韩小韩，一位对技术充满热情、涉猎广泛的探索者，同时也是一名热衷于探索前沿技术的实践者。"
+    },
+    {
+      "name": "Arthals",
+      "link": "https://arthals.ink/",
+      "avatar": "https://arthals.ink/_astro/avatar.GIIXvL5E_2e5CCN.webp",
+      "descr": "你好，我叫 Arthals，是一个兴趣使然、热爱编程的医学生。"
+    },
+    {
+      "name": "JohnieXu",
+      "link": "https://johniexu.github.io/",
+      "avatar": "https://johniexu.github.io/_astro/avatar.Ce_YfcYJ_8mxx2.webp",
+      "descr": "你好，我叫 JohnieXu，具有 9 年前端开发经验（2016-至今）。"
+    },
+    {
+      "name": "锦恢",
+      "link": "https://kirigaya.cn/home",
+      "avatar": "https://kirigaya.cn/favicon.png",
+      "descr": "我是黄哲龙/锦恢，早上好，中午好，以及晚上好！作为一名 00 后软件工程师，幽默和咖啡总是人生的必需品，所以我想要这么开头。 "
+    }, 
+    {
+      "name": "Innei",
+      "link": "https://kirigaya.cn/home",
+      "avatar": "https://innei.in/_next/image?url=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F41265413%3Fv%3D5&w=256&q=75",
+      "descr": "Hi, I'm Innei 👋 I orchestrate ideas into products with ✦AI Agents."
+    },             
   ]
 }
